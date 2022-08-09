c9e7027e9eb0376b5b3498c8f3e82ed98658841b
floralb <floralb.xhelaj@coders.al>
2022-08-09 10:29:12 +0200

    add new angular app

---
+++ apps/energy-insights-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nrg-root',
+  template: ` <nrg-nx-welcome></nrg-nx-welcome> `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'energy-insights-app';
+}
