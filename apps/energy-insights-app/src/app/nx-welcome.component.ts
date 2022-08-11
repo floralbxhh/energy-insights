e5b24b9fd3d567ea4986267bd91e0f254643e764
floralb <floralb.xhelaj@coders.al>
2022-08-11 14:51:03 +0200

    refactor: move styles in separate lib

--- apps/energy-insights-app/src/app/nx-welcome.component.ts
+++ apps/energy-insights-app/src/app/nx-welcome.component.ts
@@ -441,7 +441,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
         <div id="hero" class="rounded">
           <div class="text-container">
             <h2>
-              <svg
+              <!-- <svg
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -453,7 +453,8 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
                   stroke-width="2"
                   d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
                 />
-              </svg>
+              </svg> -->
+              <img alt="white cloud" src="/assets/images/white-cloud.svg" />
               <span>You&apos;re up and running</span>
             </h2>
             <a href="#commands"> What&apos;s next? </a>
