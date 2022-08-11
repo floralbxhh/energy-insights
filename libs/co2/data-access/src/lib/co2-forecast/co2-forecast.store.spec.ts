75dfd7f2a88eb42e34bbfed50280541320d06950
floralb <floralb.xhelaj@coders.al>
2022-08-11 11:02:30 +0200

    feat: Add endpoint call and update tests

--- libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-forecast.store.spec.ts
@@ -1,17 +1,23 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { lastValueFrom } from 'rxjs';
+import { lastValueFrom, of } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { CO2ForecastStore } from './co2-forecast.store';
+import { CO2PrognosisHttp } from './co2-prognosis.http.service';
 
 describe(CO2ForecastStore.name, () => {
   function setup() {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [CO2ForecastStore],
     });
 
+    const http = TestBed.inject(CO2PrognosisHttp);
+    const getSpy = jest.spyOn(http, 'get').mockReturnValue(of([]));
     const store = TestBed.inject(CO2ForecastStore);
 
     return {
+      getSpy,
       store,
     };
   }
--- libs/co2/data-access/src/lib/co2-forecast/co2-prognosis.http.service.spec.ts
+++ libs/co2/data-access/src/lib/co2-forecast/co2-prognosis.http.service.spec.ts
@@ -1,16 +1,76 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { lastValueFrom } from 'rxjs';
 import { CO2PrognosisHttp } from './co2-prognosis.http.service';
+import { CO2EmisResponse } from './co2-prognosis.model';
+
 describe(CO2PrognosisHttp.name, () => {
+  let controller: HttpTestingController;
   let http!: CO2PrognosisHttp;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    controller = TestBed.inject(HttpTestingController);
     http = TestBed.inject(CO2PrognosisHttp);
   });
 
-  it('works', async () => {
-    const result = await lastValueFrom(http.get());
+  afterEach(() => {
+    controller.verify();
+  });
 
-    expect(result).toEqual([]);
+  it('emits array of records', async () => {
+    //Arrange
+    const testResponse: CO2EmisResponse = {
+      total: 10,
+      sort: '',
+      limit: 10,
+      dataset: '',
+      records: [],
+    };
+
+    //Act
+    const result = lastValueFrom(http.get());
+    const req = controller.expectOne(
+      request =>
+        request.method === 'GET' &&
+        request.url.startsWith(
+          'https://api.energidataservice.dk/dataset/CO2Emis'
+        )
+    );
+    req.flush(testResponse);
+
+    //Assert
+    expect(await result).toEqual([]);
+  });
+
+  it('emits error when no records', async () => {
+    //Arrange
+    const testResponse: CO2EmisResponse = {
+      total: 10,
+      sort: '',
+      limit: 10,
+      dataset: '',
+      records: null,
+    };
+
+    //Act
+    const result = lastValueFrom(http.get());
+    const req = controller.expectOne(
+      request =>
+        request.method === 'GET' &&
+        request.url.startsWith(
+          'https://api.energidataservice.dk/dataset/CO2Emis'
+        )
+    );
+    req.flush(testResponse);
+
+    //Assert
+    expect(result).rejects.toEqual(expect.any(Error));
   });
 });
